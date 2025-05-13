d1c19dbcced701029618b0c4d11680d964e60ec3
Yurii Zatoka <yuriy.zatoka@gmail.com>
2025-05-13 13:23:01 -0700

    Final fixes

--- app/(root)/product/[id]/page.tsx
+++ app/(root)/product/[id]/page.tsx
@@ -1,8 +1,6 @@
 import { Container, ProductForm } from '@/shared/components/shared';
 import { prisma } from '@/prisma/prisma-client';
 import { notFound } from 'next/navigation';
-import { useCartStore } from '@/shared/store';
-import toast from 'react-hot-toast';
 
 export default async function ProductPage({ params: { id } }: { params: { id: string } }) {
 	const product = await prisma.product.findFirst({
